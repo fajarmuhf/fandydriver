b8c8911c051db09ae72e216a55127811bfae30a0
Fajar Muhammad Fathirochman <fajarmuhf@gmail.com>
2020-12-21 22:30:20 +0700

    hola

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+console.log("outside io");
+
+io.on('connection', function(socket){
+
+  console.log('User Conncetion');
+
+  socket.on('connect user', function(user){
+    console.log("Connected user ");
+    io.emit('connect user', user);
+  });
+
+  socket.on('on typing', function(typing){
+    console.log("Typing.... ");
+    io.emit('on typing', typing);
+  });
+
+  socket.on('chat message', function(msg){
+    console.log("Message " + msg['message']);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
